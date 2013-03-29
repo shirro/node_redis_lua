21e1c94111ec6f4c472ba1c0727fc35bb26f11a2
Amos Barreto <amos.barreto@gmail.com>
2013-03-28 19:18:07 -0700

    Update for change to send_command interface

--- redis-lua.js
+++ redis-lua.js
@@ -1,20 +1,16 @@
 
-var 
+var
   createHash = require('crypto').createHash,
   sha = function(str) {return createHash('sha1').update(str).digest('hex');};
 
 function eval_cmd(db, script, params, cb) {
   params.unshift(script);
-  params.unshift('eval');
-  params.push(cb);
-  db.send_command.apply(db, params);
+  db.send_command.call(db, 'eval', params, cb);
 }
 
 function evalsha_cmd(db, script_sha, params, cb) {
   params.unshift(script_sha);
-  params.unshift('evalsha');
-  params.push(cb);
-  db.send_command.apply(db, params);
+  db.send_command.call(db, 'evalsha', params, cb);
 }
 
 function keyval(cb) {
@@ -33,9 +29,9 @@ function keyval(cb) {
       for(i=0; i<res.length; i +=2) {
         key = res[i].toString();
         val = res[i+1];
-        hash[key] = val;   
+        hash[key] = val;
       }
-      cb(null, hash); 
+      cb(null, hash);
     }
   };
 }
@@ -44,7 +40,14 @@ exports.attachLua = function(redis) {
   redis.lua = function(name, num_keys, script, keyed) {
     var
       script_sha;
-    
+
+    function eval_cmd_callback(cb, err, res) {
+        if (!err) {
+            script_sha = script_sha || sha(script);
+        }
+        cb(err, res);
+    }
+
     redis.RedisClient.prototype[name] = function() {
       var
         cb,
@@ -68,14 +71,14 @@ exports.attachLua = function(redis) {
       if (script_sha) {
         evalsha_cmd(that, script_sha, params, function(err, res) {
           if (err && err.message.indexOf('NOSCRIPT') > 0) {
-            eval_cmd(that, script, params.slice(2, -1), cb);
+            script_sha = undefined;
+            eval_cmd(that, script, params.slice(1, -1), eval_cmd_callback.bind(null, cb));
           } else {
             cb(err, res);
           }
         });
       } else {
-        script_sha = script_sha || sha(script);
-        eval_cmd(that, script, params, cb);
+        eval_cmd(that, script, params, eval_cmd_callback.bind(null, cb));
       }
     };
   };
